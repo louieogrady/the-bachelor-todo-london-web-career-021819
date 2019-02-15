53314dbca81ebc870cc41a6551d39a9e6042103d
Louie O'Grady <l.ogrady@gmail.com>
2019-02-15 14:21:27 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -4,7 +4,7 @@ def get_first_name_of_season_winner(data, season)
   data.each do |season_num, info|
     info.each do |cont_info|
       if season_num == season && cont_info["status"] == "winner"
-        puts cont_info["name"]
+        cont_info["name"]
     end 
   end 
 end 
