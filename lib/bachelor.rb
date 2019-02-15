3b8242b471d3a1bb6b315fa534c87072e1989bce
Louie O'Grady <l.ogrady@gmail.com>
2019-02-15 14:09:17 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -2,9 +2,9 @@ require 'pry'
 
 def get_first_name_of_season_winner(data, season)
   data.each do |season_num, info|
-    binding.pry
   if season_num == season && info[:status] == "winner"
     puts data[:name]
+        binding.pry
     end 
   end 
 end 
